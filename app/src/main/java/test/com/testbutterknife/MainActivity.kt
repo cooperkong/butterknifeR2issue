72a49d4fda544aff787b70ad86192bef2738ebeb
WenChao Kong <wenchao.kong@gmail.com>
2019-04-15 13:53:20 +1000

    simualte crash

--- app/src/main/java/test/com/testbutterknife/MainActivity.kt
+++ app/src/main/java/test/com/testbutterknife/MainActivity.kt
@@ -3,7 +3,8 @@ package test.com.testbutterknife
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import android.widget.Button
+import butterknife.ButterKnife
+import butterknife.OnClick
 import test.com.lib2.Activity2
 
 
@@ -12,8 +13,11 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.mainactivity)
-        findViewById<Button>(R.id.button).setOnClickListener {
-            startActivity(Intent(this, Activity2::class.java))
-        }
+        ButterKnife.bind(this)
+    }
+
+    @OnClick(R.id.button)
+    fun donothing() {
+        startActivity(Intent(this, Activity2::class.java))
     }
 }
